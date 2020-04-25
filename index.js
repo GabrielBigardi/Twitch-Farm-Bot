e1ffaa87b1734223c09fd578c90e78bf8fb008e7
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-04-24 21:19:54 -0300

    Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const tmi = require('tmi.js');
+const clientTwitch = new tmi.Client({
+	options: { debug: true },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.username
+		password: process.env.password
+	},
+	channels: [ 'fnxlntc',
+		'saullo',
+		'fkswp',
+		'piripakyz',
+		'p4ndaotv',
+		'stereonline',
+		'rubini',
+		'mch_agg',
+		'surskity',
+		'eliastibianodoido',
+		'player_dbr',
+		'rabiscoart',
+		'marcaorx',
+		'metyzera',
+		'shktt',
+		'cogu',
+		'murilo_rt',
+		'jschritte',
+		'tutu_dias'
+	]
+});
+clientTwitch.connect();
+clientTwitch.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!testecomandao') {
+		clientTwitch.say(channel, `@${tags.username}, salve!`);
+	}
+});
