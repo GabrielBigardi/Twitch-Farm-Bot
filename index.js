eda0894611c2ba2e9577bd0b0ed4cb704bb45343
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2020-04-24 21:28:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const clientTwitch = new tmi.Client({
 		username: process.env.username
 		password: process.env.password
 	},
-	channels: [ 'fnxlntc',
+	channels: [ 'teste',
 		'saullo',
 		'fkswp',
 		'piripakyz',
